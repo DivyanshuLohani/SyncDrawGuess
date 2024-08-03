f9955f67bace3876e556efe9f4bf5b97d2de3682
Divyanshu Lohani <divyanshulohani2305@gmail.com>
2024-08-03 21:44:56 +0530

    create drawing on canvas

--- src/server.ts
+++ src/server.ts
@@ -9,9 +9,22 @@ const io = new Server(server);
 app.get("/", function (req, res) {
   res.sendFile(`${process.cwd()}/static/index.html`);
 });
+app.get("/game.js", function (req, res) {
+  res.sendFile(`${process.cwd()}/static/game.js`);
+});
 
 io.on("connection", (socket) => {
-  console.log("New Connection", socket);
+  console.log("New Connection");
+
+  socket.emit("hello", "hello from server");
+
+  socket.on("hi", (x) => {
+    console.log(x);
+  });
+
+  socket.on("disconnect", (reason) => {
+    console.log(reason);
+  });
 });
 
 server.listen(3000, function () {
